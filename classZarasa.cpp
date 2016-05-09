0011fa678c0cd8577011f1398363a6e01c6f228d
Eze <eze1214@live.com.ar>
2016-05-08 23:33:36 -0300

    Clase zarasa

---
+++ classZarasa.cpp
@@ -0,0 +1,18 @@
+/*
+ * classZarasa.cpp
+ *
+ *  Created on: 8 de may. de 2016
+ *      Author: ezequiel
+ */
+
+#include "classZarasa.h"
+
+classZarasa::classZarasa() {
+	// TODO Auto-generated constructor stub
+
+}
+
+classZarasa::~classZarasa() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ classZarasa.h
@@ -0,0 +1,17 @@
+/*
+ * classZarasa.h
+ *
+ *  Created on: 8 de may. de 2016
+ *      Author: ezequiel
+ */
+
+#ifndef CLASSZARASA_H_
+#define CLASSZARASA_H_
+
+class classZarasa {
+public:
+	classZarasa();
+	virtual ~classZarasa();
+};
+
+#endif /* CLASSZARASA_H_ */
